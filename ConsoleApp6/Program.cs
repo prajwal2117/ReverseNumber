72b7fe5453671dd08a0618efa9b8cc1b679afeba
prajwal2117 <119020725+prajwal2117@users.noreply.github.com>
2023-07-15 19:15:51 +0530

    Add files via upload

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int result = 0;
+            int rem;
+
+            Console.Write("enter number for reverse: ");
+            num = Int32.Parse(Console.ReadLine());
+
+            while (num!=0)
+            {
+                rem = num % 10;
+                result = result * 10 + rem;
+                num = num / 10;
+
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
